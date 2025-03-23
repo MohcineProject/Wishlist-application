374780898595b1e79aa6eadb64fdadff98ccf5ba
Zahdi <ZahdiMohcine20@gmail.com>
2025-03-23 17:06:59 +0100

    [Progress] minor changes

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -12,17 +12,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/wishlist')]
-final class WhishlistController extends AbstractController
+final class WishlistController extends AbstractController
 {
-    #[Route(name: 'app_whishlist_index', methods: ['GET'])]
+    #[Route(name: 'app_wishlist_index', methods: ['GET'])]
     public function index(WishlistRepository $wishlistRepository): Response
     {
-        return $this->render('whishlist/index.html.twig', [
+        return $this->render('wishlist/index.html.twig', [
             'wishlists' => $wishlistRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_whishlist_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_wishlist_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $wishlist = new Wishlist();
@@ -33,24 +33,24 @@ final class WhishlistController extends AbstractController
             $entityManager->persist($wishlist);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_whishlist_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_wishlist_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('whishlist/new.html.twig', [
+        return $this->render('wishlist/new.html.twig', [
             'wishlist' => $wishlist,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_whishlist_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_wishlist_show', methods: ['GET'])]
     public function show(Wishlist $wishlist): Response
     {
-        return $this->render('whishlist/show.html.twig', [
+        return $this->render('wishlist/show.html.twig', [
             'wishlist' => $wishlist,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_whishlist_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_wishlist_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Wishlist $wishlist, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(WishlistType::class, $wishlist);
@@ -59,16 +59,16 @@ final class WhishlistController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_whishlist_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_wishlist_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('whishlist/edit.html.twig', [
+        return $this->render('wishlist/edit.html.twig', [
             'wishlist' => $wishlist,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_whishlist_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_wishlist_delete', methods: ['POST'])]
     public function delete(Request $request, Wishlist $wishlist, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$wishlist->getId(), $request->getPayload()->getString('_token'))) {
@@ -76,6 +76,6 @@ final class WhishlistController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_whishlist_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_wishlist_index', [], Response::HTTP_SEE_OTHER);
     }
 }
