cb0297979172a880b4fd9670ca3d19539d283748
Zahdi <ZahdiMohcine20@gmail.com>
2025-03-23 15:48:33 +0100

    [Progress] merging my local changes

---
+++ archive/Invitation.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvitationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvitationRepository::class)]
+class Invitation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $userId = null;
+
+    #[ORM\Column]
+    private ?bool $state = null;
+
+    #[ORM\Column]
+    private ?int $wishlistId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function isState(): ?bool
+    {
+        return $this->state;
+    }
+
+    public function setState(bool $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getWishlistId(): ?int
+    {
+        return $this->wishlistId;
+    }
+
+    public function setWishlistId(int $wishlistId): static
+    {
+        $this->wishlistId = $wishlistId;
+
+        return $this;
+    }
+}
--- src/Controller/WhishlistController.php
+++ src/Controller/WhishlistController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/whishlist')]
+#[Route('/wishlist')]
 final class WhishlistController extends AbstractController
 {
     #[Route(name: 'app_whishlist_index', methods: ['GET'])]
